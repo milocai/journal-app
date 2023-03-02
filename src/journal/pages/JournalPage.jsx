573562c2a68bab28f11e9539f2b9f17a87bde1ff
milocai <61470383+milocai@users.noreply.github.com>
2023-03-02 19:13:28 -0300

    añadir botón flotante

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,4 +1,5 @@
-import { Typography } from "@mui/material"
+import { AddOutlined } from "@mui/icons-material"
+import { IconButton, Typography } from "@mui/material"
 import { JournalLayout } from "../layout/JournalLayout"
 import { NoteView, NothingSelectedView } from "../views"
 
@@ -13,7 +14,20 @@ export const JournalPage = () => {
       {/* Nothing selected */}
       {/* <NothingSelectedView/> */}
       
-      <NoteView/>
+      {/* <NoteView/> */}
+      <IconButton
+        size="large"
+        sx={{
+          color: 'white',
+          backgroundColor: 'error.main',
+          ':hover': { backgroundColor: 'error.main', opacity: 0.9},
+          position: 'fixed',
+          right: 50,
+          bottom: 50
+        }}
+      >
+        <AddOutlined sx={{ fontSize: 30 }}/>
+      </IconButton>
     </JournalLayout>
   )
 }
