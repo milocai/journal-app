301cb0f48782aa135c4c2c2ad35837ac43ebdd3b
milocai <61470383+milocai@users.noreply.github.com>
2023-05-03 21:06:26 -0300

    image upload button in note view added

--- src/journal/views/NoteView.jsx
+++ src/journal/views/NoteView.jsx
@@ -1,9 +1,9 @@
-import { SaveOutlined } from "@mui/icons-material"
-import { Button, Grid, TextField, Typography } from "@mui/material"
+import { SaveOutlined, UploadOutlined } from "@mui/icons-material"
+import { Button, Grid, IconButton, TextField, Typography } from "@mui/material"
 import { ImageGallery } from "../components"
 import { useForm } from "../../hooks"
 import { useDispatch, useSelector } from "react-redux"
-import { useEffect, useMemo } from "react"
+import { useEffect, useMemo, useRef } from "react"
 import { setActiveNote, startSaveNote } from "../../store/journal"
 import Swal from "sweetalert2"
 import 'sweetalert2/dist/sweetalert2.css'
@@ -21,6 +21,8 @@ export const NoteView = () => {
     return newDate.toUTCString()
   }, [ date ] )
 
+  const fileInputRef = useRef()
+
   useEffect(() => {
     dispatch( setActiveNote( formState ) )  
     
@@ -37,6 +39,10 @@ export const NoteView = () => {
     dispatch( startSaveNote() )
   }
 
+  const onFileInputChange = ({ target }) => {
+    if ( target.files === 0 ) return
+    //dispatch( startUploadingFiles( target.files ))  
+  }
   
   return (
     <Grid 
@@ -51,6 +57,23 @@ export const NoteView = () => {
             <Typography fontSize={39} fontWeight='light'>{ dateString }</Typography>
         </Grid>
         <Grid item>
+            
+            <input
+              type="file"
+              multiple
+              ref={ fileInputRef }
+              onChange = { onFileInputChange }
+              style={{ display: 'none' }}
+            />
+
+            <IconButton
+              color='primary'
+              disabled={ isSaving }
+              onClick={ () => fileInputRef.current.click() }
+            >
+              <UploadOutlined/>
+            </IconButton>
+
             <Button 
               disabled={ isSaving }
               onClick={ onSaveNote }
